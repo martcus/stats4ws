6a563f2d2e4079e7c6a938d0cfdee230727bd3df
martcus <marco.lovazzano@amac02zr02wlvdc.homenet.telecomitalia.it>
2020-03-12 16:49:28 +0100

    Coding improvements

--- stats.sh
+++ stats.sh
@@ -14,16 +14,53 @@ readonly STATS4WS_BASENAME=$(basename "$0")
 readonly SAVEIFS=$IFS
 IFS=$(echo -en "\n\b") # <-- change this as it depends on your app
 
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+# set -o xtrace
+
+# Set magic variables for current file & dir
+readonly __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+readonly __base="$(basename "${__file}" .sh)"
+readonly __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
+
+# Variable
+stats4ws_debug="N"
+stats4ws_logfile="$1"
+stats4ws_filter=${2:-""}
+
+# print debug message
+# parameters:
+# 1- message to echo
+# usage: _debug "Hello, World!"
+function _debug() {
+    if [ "$stats4ws_debug" = "Y" ]; then
+        echo "DEBUG> $1"
+    fi
+}
+
+# print header
+function _header {
+    echo "log;operation;calls;min;mean;max;stdev;25perc;median;75perc"
+}
+
 #Param
 # 1 log file
 
-echo -e "log;operation;calls;min;mean;max;stdev;25perc;median;75perc"
+_header
 
 # list operations
-LIST_OPS="zgrep \"=Response\" \"$1\" |  cut -d\";\" -f5 | sed 's/targetOperation=//' | sort | uniq"
+_LIST="zgrep \"=Response.*$stats4ws_filter\" \"$stats4ws_logfile\" |  cut -d\";\" -f5 | sed 's/targetOperation=//' | sort | uniq"
 
 # loop over operations
-for ops in $(eval "$LIST_OPS"); do
+for ops in $(eval "$_LIST"); do
     _CMD="zgrep \"=Response.*$ops\" \"$1\" | cut -d\";\" -f8 | sed 's/exectime=// ; s/-->//' | sort -n"
     printf "$1;$ops;"
 	 echo $(eval "$_CMD") | tr ' ' '\n' | awk '
@@ -43,5 +80,6 @@ for ops in $(eval "$LIST_OPS"); do
 		}'
 done
 
+# Restore IFS
 IFS=$SAVEIFS
 exit 0
