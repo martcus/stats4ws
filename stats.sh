3b33dc67abfced99149996d6c95f391a23e3469b
Marco Lovazzano <marco.lovazzano@gmail.com>
2020-03-12 07:34:13 +0100

    Add stddev

--- stats.sh
+++ stats.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #--------------------------------------------------------------------------------------------------
-# avg.sh
+# stats.sh
 # Copyright (c) Marco Lovazzano
 # Licensed under the GNU General Public License v3.0
 # http://github.com/martcus
@@ -15,23 +15,31 @@ readonly SAVEIFS=$IFS
 IFS=$(echo -en "\n\b") # <-- change this as it depends on your app
 
 #Param
-# 1 file di log
+# 1 log file
 
-echo -e "log;operation;calls;mean;min;max;25perc;median;75perc"
+echo -e "log;operation;calls;min;mean;max;stdev;25perc;median;75perc"
 
-# identifico l'elenco dei servizi disponibili
+# list operations
 LIST_OPS="zgrep \"=Response\" \"$1\" |  cut -d\";\" -f5 | sed 's/targetOperation=//' | sort | uniq"
 
+# loop over operations
 for ops in $(eval "$LIST_OPS"); do
     _CMD="zgrep \"=Response.*$ops\" \"$1\" | cut -d\";\" -f8 | sed 's/exectime=// ; s/-->//' | sort -n"
-
     printf "$1;$ops;"
 	 echo $(eval "$_CMD") | tr ' ' '\n' | awk '
 		{
-			a[i++]=$0; s+=$0; all[NR]=$0
+			a[i++]=$0; s+=$0; sumsq+=$0*$0
 		}
 		END {
-			printf "%.0f;%.0f;%.0f;%.0f;%.0f;%.0f;%.0f\n", i, s/i, a[0], a[i-1], all[int(NR*0.25 + 0.5)], (a[int(i/2)]+a[int((i-1)/2)])/2, all[int(NR*0.75 + 0.5)]
+			printf "%.0f" , i
+			printf ";%.0f", a[0]
+			printf ";%.0f", s/NR
+			printf ";%.0f", a[i-1]
+			printf ";%.0f", sqrt(sumsq/NR - (s/NR)**2)
+			printf ";%.0f", a[int(NR*0.25 - 0.5)]
+			printf ";%.0f", (a[int(i/2)]+a[int((i-1)/2)])/2
+			printf ";%.0f", a[int(NR*0.75 - 0.5)]
+			printf "\n"
 		}'
 done
 
