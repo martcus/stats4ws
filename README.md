79fc0c7e945e77ed199746ee8bc38011b5514114
martcus <marco.lovazzano@amac02zr02wlvdc.homenet.telecomitalia.it>
2020-03-14 08:20:35 +0100

    Some improvements

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# stats4ws
+# stats4ws v1.0.0
+
+## Prerequisites
+Your log must be report a line before every response like this:
+```
+<!--type=Response;sessionId=null;messageId=1;targetService=Service;targetOperation=Operation;requestTime=1571379171703;responseTime=1571379172226;exectime=523-->
+```
+
+## Help
+```
+stats.sh v0.1.0
+Statistics for web services based on axis1
+Copyright (c) Marco Lovazzano
+Licensed under the GNU General Public License v3.0
+http://github.com/martcus
+
+Usage: stats.sh [OPTIONS/PARAMS]
+ Options:
+  --help      : Print this help and exit. Option cannot be used with params
+ Params
+  1- File     : Log File to analyze. First param is mandatory
+  2- Optional : Filter to apply to the log
+```
+
+## Example:
+```
+./stats.sh --help
+./stats.sh ws.log
+./stats.sh ws.log filter | column -t -s ';'
+```
--- stats.sh
+++ stats.sh
@@ -7,7 +7,7 @@
 #--------------------------------------------------------------------------------------------------
 
 readonly STATS4WS_APPNAME="stats"
-readonly STATS4WS_VERSION="1.0.0"
+readonly STATS4WS_VERSION="0.1.0"
 readonly STATS4WS_BASENAME=$(basename "$0")
 
 # IFS stands for "internal field separator". It is used by the shell to determine how to do word splitting, i. e. how to recognize word boundaries.
@@ -46,38 +46,67 @@ function _debug() {
     fi
 }
 
-# print header
-function _header {
-    echo "log;operation;calls;min;mean;max;stdev;25perc;median;75perc"
+# print help and exit with code 0
+function _help() {
+    echo -e ""
+    echo -e "$(basename "$0") v$STATS4WS_VERSION"
+    echo -e "Statistics for web services based on axis1"
+    echo -e "Copyright (c) Marco Lovazzano"
+    echo -e "Licensed under the GNU General Public License v3.0"
+    echo -e "http://github.com/martcus"
+    echo -e ""
+    echo -e "Usage: $STATS4WS_BASENAME [OPTIONS/PARAMS]"
+    echo -e " Options:"
+    echo -e "  --help      : Print this help and exit. Option cannot be used with params "
+    echo -e " Params"
+    echo -e "  1- File     : Log File to analyze. First param is mandatory"
+    echo -e "  2- Optional : Filter to apply to the log"
+    echo -e ""
+    echo -e "Usage example:"
+    echo -e "./stats.sh --help"
+    echo -e "./stats.sh ws.log"
+	echo -e "./stats.sh ws.log filter | column -t -s ';'"
 }
 
-#Param
-# 1 log file
+# print header
+function _header {
+    echo "log;operation;calls;min;mean;max;stdev;25perc;median;75perc;85perc;95perc"
+}
+
+if [ "$stats4ws_logfile" = "--help" ]; then
+    _help
+    exit 0
+fi
 
 _header
 
 # list operations
 _LIST="zgrep \"=Response.*$stats4ws_filter\" \"$stats4ws_logfile\" |  cut -d\";\" -f5 | sed 's/targetOperation=//' | sort | uniq"
+_debug "$_LIST"
 
 # loop over operations
 for ops in $(eval "$_LIST"); do
-    _CMD="zgrep \"=Response.*$ops\" \"$1\" | cut -d\";\" -f8 | sed 's/exectime=// ; s/-->//' | sort -n"
-    printf "$1;$ops;"
-	 echo $(eval "$_CMD") | tr ' ' '\n' | awk '
-		{
-			a[i++]=$0; s+=$0; sumsq+=$0*$0
-		}
-		END {
-			printf "%.0f" , i
-			printf ";%.0f", a[0]
-			printf ";%.0f", s/NR
-			printf ";%.0f", a[i-1]
-			printf ";%.0f", sqrt(sumsq/NR - (s/NR)**2)
-			printf ";%.0f", a[int(NR*0.25 - 0.5)]
-			printf ";%.0f", (a[int(i/2)]+a[int((i-1)/2)])/2
-			printf ";%.0f", a[int(NR*0.75 - 0.5)]
-			printf "\n"
-		}'
+    _CMD="zgrep \"=Response.*$ops\" \"$stats4ws_logfile\" | cut -d\";\" -f8 | sed 's/exectime=// ; s/-->//' | sort -n"
+    _debug "$_CMD"
+
+    printf "$stats4ws_logfile;$ops;"
+    echo $(eval "$_CMD") | tr ' ' '\n' | awk '
+        {
+            a[i++]=$0; s+=$0; sumsq+=$0*$0
+        }
+        END {
+            printf "%.0f" , i
+            printf ";%.0f", a[0]
+            printf ";%.0f", s/NR
+            printf ";%.0f", a[i-1]
+            printf ";%.0f", sqrt(sumsq/NR - (s/NR)**2)
+            printf ";%.0f", a[int(NR*0.25 - 0.5)]
+            printf ";%.0f", (a[int(i/2)]+a[int((i-1)/2)])/2
+            printf ";%.0f", a[int(NR*0.75 - 0.5)]
+            printf ";%.0f", a[int(NR*0.85 - 0.5)]
+            printf ";%.0f", a[int(NR*0.95 - 0.5)]
+            printf "\n"
+        }'
 done
 
 # Restore IFS
